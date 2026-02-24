835b7ee9ef7d0cc89d5865957019666027d68994
Ahmed Alhamed <ahmedalhamed1207@gmail.com>
2026-02-24 20:49:40 +0300

    chore: initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+
+      <p>Ad Soyad: Ahmed Al Hamed</p>
+      <p>Ogrenci No: 225541606</p>
+    </div>
+  )
+}
+
+export default App
